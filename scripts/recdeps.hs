754cea956879076a1638edf2d877009519fe5ff8
Magnus Therning <magnus@therning.org>
2010-10-15 14:11:03 +0100

    Promote recdeps to a tool, and add ability to pass package name on the command line.

--- scripts/recdeps.hs
+++ scripts/recdeps.hs
@@ -2,14 +2,18 @@
 -- | This test reads the current directory and dumps a topologically sorted package list
 --
 
+module Main where
+
 import Distribution.ArchLinux.SrcRepo
 import System.IO
 import System.Directory
+import System.Environment
 import Control.Monad
 
 main = do
+  [pkg] <- getArgs
   dot <- getCurrentDirectory
   repo <- getRepoFromDir dot
   case repo of
     Nothing -> return ()
-    Just r -> foldM (\a -> \s -> putStrLn s) () (getDependencies "haskell-puremd5" r)
+    Just r -> foldM (\a -> \s -> putStrLn s) () (getDependencies pkg r)
