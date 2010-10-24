4caaff68b654c02aa97e95e70a1f186911ce06b2
Peter Simons <simons@cryp.to>
2010-10-24 22:36:14 +0200

    getSpecifiedCabalsFromTarball: fail fatally if a package descriptor doesn't parse
    Previously, Debug.Trace.trace was used to write out a warning, but in
    the plethora of output cabal2arch generates this warnings tends to be
    missed. Now, such an error triggers an exception.

--- Distribution/ArchLinux/HackageTranslation.hs
+++ Distribution/ArchLinux/HackageTranslation.hs
@@ -49,21 +49,19 @@ parse2maybe a = case a of
 --
 getSpecifiedCabalsFromTarball :: Bytes.ByteString -> [String] -> [GenericPackageDescription]
 getSpecifiedCabalsFromTarball tarball list =
-  getSpecifiedCabals (mapMaybe parsePackageIdentifier list) (getCabalsFromTarball tarball)
+  getSpecifiedCabals (map parsePackageIdentifier list) (getCabalsFromTarball tarball)
 
 --
 -- | Parses a list of lines of the form "package-name 1.2.3.4"
 --
-parsePackageIdentifier :: String -> Maybe PackageIdentifier
+parsePackageIdentifier :: String -> PackageIdentifier
 parsePackageIdentifier s = case words s of
-  name:version:_ -> case ver of
+  name:version:_ -> case simpleParse version of
         Nothing -> void
-        Just v -> Just $ PackageIdentifier { pkgName = PackageName name ,
-                                       pkgVersion = v }
-      where ver = simpleParse version
+        Just v -> PackageIdentifier { pkgName = PackageName name, pkgVersion = v }
   _ -> void
  where
-  void = Debug.Trace.trace("Malformed package identifier " ++ show s) Nothing
+  void = error ("Malformed package identifier " ++ show s)
 
 getSpecifiedCabals :: [PackageIdentifier] -> [GenericPackageDescription] -> [GenericPackageDescription]
 getSpecifiedCabals list packages = filter wasSpecified packages
