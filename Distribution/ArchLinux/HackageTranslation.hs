4eea0250c58a7eab37222b238d2a2cf2bd664012
Rémy Oudompheng <oudomphe@math.unice.fr>
2010-10-26 16:55:41 +0200

    CabalTranslation: relax constraints on pkglist syntax to allow blank lines and comments (with "# ")

--- Distribution/ArchLinux/HackageTranslation.hs
+++ Distribution/ArchLinux/HackageTranslation.hs
@@ -49,19 +49,22 @@ parse2maybe a = case a of
 --
 getSpecifiedCabalsFromTarball :: Bytes.ByteString -> [String] -> [GenericPackageDescription]
 getSpecifiedCabalsFromTarball tarball list =
-  getSpecifiedCabals (map parsePackageIdentifier list) (getCabalsFromTarball tarball)
+  getSpecifiedCabals (mapMaybe parsePackageIdentifier list) (getCabalsFromTarball tarball)
 
 --
 -- | Parses a list of lines of the form "package-name 1.2.3.4"
 --
-parsePackageIdentifier :: String -> PackageIdentifier
+parsePackageIdentifier :: String -> Maybe PackageIdentifier
 parsePackageIdentifier s = case words s of
+  []             -> void
+  "#":_          -> void
   name:version:_ -> case simpleParse version of
-        Nothing -> void
-        Just v -> PackageIdentifier { pkgName = PackageName name, pkgVersion = v }
-  _ -> void
+        Nothing -> err
+        Just v -> Just $ PackageIdentifier { pkgName = PackageName name, pkgVersion = v }
+  _ -> err
  where
-  void = error ("Malformed package identifier " ++ show s)
+  void = Nothing
+  err = error ("Malformed package identifier " ++ show s)
 
 getSpecifiedCabals :: [PackageIdentifier] -> [GenericPackageDescription] -> [GenericPackageDescription]
 getSpecifiedCabals list packages = filter wasSpecified packages
